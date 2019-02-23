df5f8c117eefbc81ab12d80fdaebd34d8b0235e2
Andrii Kudlaienko <andrii.kudlaienko@godaddy.com>
2019-02-23 17:42:46 +0200

    [vmcreate] - get info

--- vmcreate.py
+++ vmcreate.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python2.7
 
 import os
+import pdb
+import sys
 import select
 import prlsdkapi
-from prlsdkapi import consts
+from prlsdkapi import *
 
+from pprint import pprint
+
+
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 # Initialize the SDK.
 def init():
-    print "init_server_sdk"
+    #print "init_server_sdk"
     prlsdkapi.init_server_sdk()
 
 
@@ -22,7 +30,7 @@ def deinit():
 def create(srv, name, os_template):
     print 'create ct'
     ct = srv.create_vm()
-    ct.set_vm_type(consts.PVT_CT)
+    ct.set_vm_type(consts.PVT_VM)
     ct.set_name(name)
     ct.set_os_template(os_template)
     ct.reg('', True).wait()
@@ -147,34 +155,194 @@ def create_ct(ct_name, ct_os_template, ct_cpu_limit, ct_ram):
     ct2.delete()
     srv.logoff().wait()
     deinit()
+#################
+def get_vm_os_info(vm):
+
+ print("\n-----------------------------")
+ # Virtual server name.
+ print("Virtual server name: " + vm.get_name())
+
+ # Obtain the VmConfig object containing the virtual server
+ # configuration information.
+ vm_config = vm.get_config()
+ # Obtain the guest OS type and version.
+ # OS types are defined as PVS_GUEST_TYPE_xxx constants.
+ # For the complete list, see the documentation for
+ # the prlsdkapi.prlsdk.consts module or
+ # the Virtuozzo Python API Reference guide.
+ os_type = vm_config.get_os_type()
+ if os_type == consts.PVS_GUEST_TYPE_WINDOWS:
+     osType = "Windows"
+ elif os_type == consts.PVS_GUEST_TYPE_LINUX:
+     osType = "Linux"
+ else:
+     osType = "Other type (" + str(os_type) + ")"
+
+ # OS versions are defined as PVS_GUEST_VER_xxx constants.
+ os_version = vm_config.get_os_version()
+ if os_version == consts.PVS_GUEST_VER_WIN_WINDOWS8:
+     osVersion = "Windows 8"
+ elif os_version == consts.PVS_GUEST_VER_LIN_CENTOS:
+     osVersion = "CentOS"
+ else:
+     osVersion = "Other version (" + str(os_version) + ")"
+# print "Guest OS: " + osType + " " + osVersionRAM size
+# p.115 - 130 !!!!!!!!!!
 
 
-### Create a Virtual Machine ###
-def create(server):
-    # Get the ServerConfig object.
-    result = server.get_srv_config().wait()
-    srv_config = result.get_param()
-    # Create a Vm object and set the virtual server type
-    # to Virtual Machine (PVT_VM).
-    vm = server.create_vm()
-    vm.set_vm_type(consts.PVT_VM)
-    # Set a default configuration for the VM.
-    vm.set_default_config(srv_config, consts.PVS_GUEST_VER_WIN_WINDOWS8, True)
-    # Set the VM name.
-    vm.set_name("Windows8")
-    # Modify the default RAM and HDD size.
-    vm.set_ram_size(256)
-    dev_hdd = vm.get_hard_disk(0)
-    dev_hdd.set_disk_size(3000)
-    # Register the VM with the Virtuozzo.
-    vm.reg("", True).wait()
+###
+def createvm(vm_name, vm_description, vm_ram_size, vm_hdd_size):
+    init()
+    server = prlsdkapi.Server()
+    server.login_local().wait()
+    #server.login('127.0.0.1', 'user', 'test_user').wait()
+# Obtain the prlsdkapi.ServerConfig object.
+# The object contains the host server configuration information.
+    job_vm_list = server.get_vm_list_ex(consts.PVTF_VM | consts.PVTF_CT)
+    #job_vm_list = server.get_vm_list()  # returns just a list of VM
+    job_vm_list.wait()
+    result = job_vm_list.get_result()
+    members_info = {}
+    # Iterate through the Result object parameters.
+    # Each parameter is an instance of the prlsdkapi.Vm class.
+    for i in range(result.get_params_count()):
+        member = result.get_param_by_index(i)
+        # Obtain the prlsdkapi.VmConfig object containing the virtual server
+        # configuration information.
+        member_config = member.get_config()
+        # Get the name of the virtual server.
+        params = {"get_name": 'get_name()'}
+        #for param in params:
 
-# Change VM name.
-def setName(vm, name):
-    print 'set_name'
-    vm.begin_edit().wait()
-    vm.set_name(name)
-    vm.commit().wait()
+        member_name = member_config.get_name()
+        member_hostname = member_config.get_hostname()
+        member_description = member_config.get_description()
+        member_cpu_count = member_config.get_cpu_count()
+        member_ram = member_config.get_ram_size()
+        member_auto_start = member_config.get_auto_start()
+        member_vnc = member_config.get_vncmode()
+        #member_hdd = member_config.get_hard_disk()
+        #member_gateway = member_config.get_default_gateway()
+        member_dns_raw = member_config.get_dns_servers()
+        member_search_domains_raw = member_config.get_search_domains()
+        member_search_domains = []
+        member_dns = []
+        for d in member_dns_raw:
+            member_dns.append(d)
+        for s in member_search_domains_raw:
+            member_search_domains.append(s)
+
+        # CPU usage limit (in percent) for a virtual machine.
+        #member_cpu_limit = member_config.get_cpu_limit()
+        # Determine the number of CPU units allocated to a virtual machine.
+        member_cpu_units = member_config.get_cpu_units()
+        member_type = member_config.get_vm_type()
+        member_is_template = member_config.is_template()
+
+        #member_disk_size =
+        if member_type == consts.PVT_VM:
+            member_type_is = "VM"
+        elif member_type == consts.PVT_CT:
+            member_type_is = "CT"
+        else:
+            member_type_is = "UNKNOWN"
+        # p. 91 *_api_reference.*
+        member_info = {"hostname": member_hostname, "description": member_description, "cpu_count": member_cpu_count,
+                       "ram": member_ram, "auto_start": member_auto_start, "type": member_type_is,
+                       "template": member_is_template, "vnc": member_vnc, "search_domains": member_search_domains,
+                       "hdd": "", "dns": member_dns}
+        members_info.update({member_name: member_info})
+    for name, details in members_info.items():
+        print("\nname: {0}\n{1}\n***************************************************".format(name, details))
+    # try:
+    #     result = server.get_srv_config().wait()
+    #     print(result)
+    # except prlsdkapi.PrlSDKError, e:
+    #     print("Error: {}".format(e))
+    #
+    # srv_config = result.get_param()
+    # print(srv_config)
+
+# Create a new prlsdkapi.Vm object.
+#     vm = server.create_vm()
+#
+# # Set the virtual server type (PVT_VM - Virtual Machine;  PVT_CT - Container)
+#     vm.set_vm_type(consts.PVT_VM)
+#
+# # Use the default configuration for the Virtual Machine.
+# # Parameters of the set_default_config method:
+# # param_1: The host server configuration object.
+# # param_2: Target OS type and version.
+# # param_3: Specifies to create the virtual server devices using
+# # default values (the settings can be modified
+# # later if needed).
+# # centOS: PVS_GUEST_VER_LIN_CENTOS
+# # Windows8: PVS_GUEST_VER_WIN_WINDOWS8
+# #     if srv_config is None or srv_config == "":
+# #         print("\n * 'srv_config' is empty!\n")
+# #         sys.exit(1)
+# #     else:
+#     vm.set_default_config(srv_config, consts.PVS_GUEST_VER_LIN_CENTOS, True)
+#
+# # Set the virtual server name and description.
+#     vm.set_name(vm_name)
+#     vm.set_description(vm_description)
+#
+# # Modify the default RAM size and HDD size.
+# # These two steps are optional. If you omit them, the
+# # default values will be used.
+# # vm_ram_size  in MB
+#     vm.set_ram_size(vm_ram_size)
+#
+# # Set HDD size to 10 gig.
+# # The get_device method obtains a prlsdkapi.VmHardDisk object.
+# # The index 0 is used because the default configuration has a
+# # single hard disk.
+# # vm_hdd_size in MB
+#     dev_hdd = vm.get_hard_disk(0)
+#     dev_hdd.set_disk_size(vm_hdd_size)
+#
+# # Register the virtual server with the Virtuozzo host.
+# # The first parameter specifies to create the server in the
+# # default directory on the host computer.
+# # The second parameter specifies that non-interactive mode
+# # should be used.
+#     print "Creating a virtual server..."
+#     try:
+#         vm.reg("", True).wait()
+#     except prlsdkapi.PrlSDKError, e:
+#         print("Error: {}".format(e))
+#         return
+#     print "Virtual server was created successfully."
+
+
+# ##############
+# ### Create a Virtual Machine ###
+# def create(server):
+#     # Get the ServerConfig object.
+#     result = server.get_srv_config().wait()
+#     srv_config = result.get_param()
+#     # Create a Vm object and set the virtual server type
+#     # to Virtual Machine (PVT_VM).
+#     vm = server.create_vm()
+#     vm.set_vm_type(consts.PVT_VM)
+#     # Set a default configuration for the VM.
+#     vm.set_default_config(srv_config, consts.PVS_GUEST_VER_WIN_WINDOWS8, True)
+#     # Set the VM name.
+#     vm.set_name("Windows8")
+#     # Modify the default RAM and HDD size.
+#     vm.set_ram_size(2048)
+#     dev_hdd = vm.get_hard_disk(0)
+#     dev_hdd.set_disk_size(10000)
+#     # Register the VM with the Virtuozzo.
+#     vm.reg("", True).wait()
+#
+# # Change VM name.
+# def setName(vm, name):
+#     print 'set_name'
+#     vm.begin_edit().wait()
+#     vm.set_name(name)
+#     vm.commit().wait()
 
 # Set boot device priority
 def vm_boot_priority(vm):
@@ -229,7 +397,10 @@ def execute(vm):
     vm_guest.logout().wait()
 
 
+
+
 ### MAIN ###
 
 if __name__ == "__main__":
-    create_ct("py_test1", 'centos-7-x86_64', 50, 1024)
+#    create_ct("testct_VM", 'CentOS7-man-net', ct_cpu_limit, ct_ram)
+    createvm("test2_centos7", "test_centos7 via API", 2048, 10000)
